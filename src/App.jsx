b4d40d2ee26f15a4034779285190fb5b4d88962f
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2026-02-24 11:15:07 +0100

    Add styled-components, global styles, and basic UI components
    - Configured `styled-components` for styling.
    - Added `GlobalStyles` with a theme and default styles.
    - Created `Button` and `Input` components with customizable styles.
    - Updated `App` to showcase new components and global styles.
    - Imported Google Fonts for improved typography.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
+
+const H1 = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button
+          type={"button"}
+          onClick={() => alert("Checked in successfully. Enjoy your stay.")}
+        >
+          Check In
+        </Button>
+        <Button
+          type={"button"}
+          onClick={() => alert("Checked out successfully.")}
+        >
+          Check Out
+        </Button>
+        <Input type={"number"} placeholder={"Number of guests"} />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
