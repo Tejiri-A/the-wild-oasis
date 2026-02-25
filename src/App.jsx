35f5f8f9de95c5b02583d6b7decfb3a04d52dd2a
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2026-02-25 09:45:08 +0100

    Integrate React Query for data management and debugging
    - Added `@tanstack/react-query` and `@tanstack/react-query-devtools` dependencies.
    - Wrapped `App` in `QueryClientProvider` with React Query configuration.
    - Integrated `ReactQueryDevtools` for debugging.
    - Updated `pnpm-lock.yaml` and `package.json` to reflect new dependencies.
    - Added `pnpm-workspace.yaml` to ignore built dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,39 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import AppLayout from "./ui/AppLayout.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to={"dashboard"} />} />
-            <Route path={"dashboard"} element={<Dashboard />} />
-            <Route path={"bookings"} element={<Bookings />} />
-            <Route path={"cabins"} element={<Cabins />} />
-            <Route path={"users"} element={<Users />} />
-            <Route path={"settings"} element={<Settings />} />
-            <Route path={"account"} element={<Account />} />
-          </Route>
-          <Route path={"login"} element={<Login />} />
-          <Route path={"*"} element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to={"dashboard"} />} />
+              <Route path={"dashboard"} element={<Dashboard />} />
+              <Route path={"bookings"} element={<Bookings />} />
+              <Route path={"cabins"} element={<Cabins />} />
+              <Route path={"users"} element={<Users />} />
+              <Route path={"settings"} element={<Settings />} />
+              <Route path={"account"} element={<Account />} />
+            </Route>
+            <Route path={"login"} element={<Login />} />
+            <Route path={"*"} element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
